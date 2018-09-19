f3ba7813c4d4b103aade5bd497343914e4a9b979
Rob Goelz <RobGoelz@gmail.com>
2018-09-19 16:57:36 -0400

    feat: add solution from Udacity

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@ Instructions:
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
-(function(document) {
+(function (document) {
   'use strict';
 
   var home = null;
@@ -19,7 +19,7 @@ Instructions:
    * Helper function to show the search query.
    * @param {String} query - The search query.
    */
-  function addSearchHeader(query) {
+  function addSearchHeader (query) {
     home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
   }
 
@@ -27,7 +27,7 @@ Instructions:
    * Helper function to create a planet thumbnail.
    * @param  {Object} data - The raw data describing the planet.
    */
-  function createPlanetThumb(data) {
+  function createPlanetThumb (data) {
     var pT = document.createElement('planet-thumb');
     for (var d in data) {
       pT[d] = data[d];
@@ -40,10 +40,8 @@ Instructions:
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
-  function get(url) {
-    return fetch(url, {
-      method: 'get'
-    });
+  function get (url) {
+    return fetch(url, {method: 'get'});
   }
 
   /**
@@ -51,22 +49,30 @@ Instructions:
    * @param  {String} url - The JSON URL to fetch.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
-  function getJSON(url) {
-    return get(url).then(function(response) {
+  function getJSON (url) {
+    return get(url).then(function (response) {
       return response.json();
     });
   }
 
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', function () {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Refactor this code!
-     */
+
     getJSON('../data/earth-like-results.json')
-    .then(function(response) {
-      response.results.forEach(function(url) {
-        getJSON(url).then(createPlanetThumb);
+      .then(function (response) {
+        var sequence = Promise.resolve();
+
+        addSearchHeader(response.query);
+
+        response.results.forEach(function (url) {
+          sequence = sequence.then(function () {
+            return getJSON(url);
+          })
+            .then(createPlanetThumb);
+        });
+      })
+      .catch(function (e) {
+        console.log(e);
       });
-    });
   });
 })(document);
